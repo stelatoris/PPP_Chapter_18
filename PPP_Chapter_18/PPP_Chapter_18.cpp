4c65ae35e6ffa7a20bcd78992b6ec609ccdf8e44
stelatoris <78478872+stelatoris@users.noreply.github.com>
2022-01-04 22:20:59 +0100

    Ex 08 done.

--- PPP_Chapter_18/PPP_Chapter_18.cpp
+++ PPP_Chapter_18/PPP_Chapter_18.cpp
@@ -12,38 +12,64 @@
 
 #include "std_lib_facilities.h"
 
-string is_palindrome(const string& s)
-{
-    
+bool is_palindrome(const string& s)
+{    
     int n = s.length();
     string backwards;
-    for (int i = n; i > -1; --i) {
+    for (int i = n-1; i > -1; --i) {    //backward copy
         backwards.push_back(s[i]);
     }
-   
-    return backwards;
-}
 
+    for (int i = 0; i < n; ++i) {    // compare
+        if (backwards[i] != s[i]) return false;
+    }
+   
+    return true;
+}
 
 bool is_palindrome(const char s[], int n)
 {
-    int first = 0;
-    int last = n - 1;
-    while (first < last) {
-        if (s[first] != s[last]) return false;
-        ++first;
-        --last;
+    char* c_copy = new char[n+1];
+    c_copy[n] = 0;
+    int c_i{ n-1 };
+    // copy backwards
+    for (int i = 0; i < n; ++i) {
+        c_copy[i] = s[c_i];
+        --c_i;
     }
+
+    // compare
+    for (int i = 0; i < n; ++i) {
+        if (c_copy[i] != s[i]) {
+            delete[] c_copy;
+            return false;
+        }
+            
+    }
+    delete[] c_copy;
     return true;
 }
 
 bool is_palindrome(const char* first, const char* last)
 {
-    while (first < last) {
-        if (*first != *last) return false;
-        ++first;
+    int c_size = last - first + 1;
+    char* c_copy = new char[c_size+1];
+    char* c_copy_ptr = c_copy;
+    
+    while (last>=first) {
+        *c_copy_ptr = *last;
+        ++c_copy_ptr;
         --last;
     }
+    *c_copy_ptr = 0;
+    
+    for (int i = 0; i < c_size; ++i) {
+        if (c_copy[i] != first[i]) {
+            delete[] c_copy;
+            return false;
+        }
+    }
+    delete[] c_copy;
     return true;
 }
 
@@ -58,8 +84,42 @@ istream& read_word(istream& is, char* buffer, int max)
 int main()
 try
 {
-    string s1{ "hasan" };
-    cout<<is_palindrome(s1);
+    
+    // 18.7.1 Palindromes using string
+    /*
+    cout << "Enter a string:\n";
+    for (string s; cin >> s; ) {
+        cout << s << " is";
+        if (!is_palindrome(s)) cout << " not";
+        cout << " a palindrome\n";
+    }
+    */
+    
+    
+    // 18.7.2 Palindromes using arrays
+    /*
+    constexpr int max = 128;
+    cout << "Enter a string:\n";
+    for (char s[max]; read_word(cin, s, max); ) {
+        cout << s << " is";
+        if (!is_palindrome(s, strlen(s))) cout << " not";
+        cout << " a palindrome\n";
+    }
+    */
+
+
+
+    // 18.7.3 Palindromes using pointers
+    ///*
+    const int max = 128;
+    cout << "Enter a string:\n";
+    for (char s[max]; read_word(cin, s, max); ) {
+        cout << s << " is";
+        if (!is_palindrome(&s[0], &s[strlen(s)-1])) cout << " not";
+        cout << " a palindrome\n";
+    }
+    //*/
+
 }
 
 catch (std::exception& e) {
